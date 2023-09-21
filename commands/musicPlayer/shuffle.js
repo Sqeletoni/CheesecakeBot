8ebf1e4dd0bc66dac336f4ad4ad62fd9e04b70db
sqeleton <s1149501@student.hsleiden.nl>
2023-09-21 15:29:10 +0200

    added command: nowplaying.js added command: shuffle.js

--- commands/musicPlayer/shuffle.js
+++ commands/musicPlayer/shuffle.js
@@ -2,14 +2,14 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("quit")
-        .setDescription("stops bot and clears queue"),
+        .setName("shuffle")
+        .setDescription("shuffle the queue"),
     run: async ({ client, interaction}) => {
         const queue = client.player.getQueue(interaction.guildId)
 
         if (!queue) return await interaction.editReply("There are no songs in the queue")
 
-        queue.destroy()
-        await interaction.editReply("CYA")
+        queue.shuffle()
+        await interaction.editReply("queue is shuffled")
     }
 }
